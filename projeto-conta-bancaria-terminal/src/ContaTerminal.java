b1b3f3a4575632800ea6c0a20d464e1e993a7de1
Victor Muller <vitorsilvamuller02@gmail.com>
2025-03-05 21:53:02 -0300

    Finalização do desafio

--- projeto-conta-bancaria-terminal/src/ContaTerminal.java
+++ projeto-conta-bancaria-terminal/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //criando objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //variaveis
+        int numero = 1021;
+        double saldo = 300.50;
+        
+        System.out.println("Digite o seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o número da Agência");
+        String agencia = scanner.next();
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque!");
+
     }
 }
